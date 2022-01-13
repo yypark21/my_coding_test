6c1461ffe7b1a4dca056a4c1e07a55e343587ab0
unknown <yypark1695@gmail.com>
2022-01-13 17:10:17 +0900

    Changes to be committed: 	deleted:    1.md 	deleted:    1.py 	deleted:    2.md 	deleted:    2.py 	deleted:    3.md 	deleted:    3.py 	deleted:    4.md 	deleted:    4.py 	deleted:    5.md

--- 3.py
+++
@@ -1,6 +0,0 @@
-import math
-def solution(w,h):
-        return w * h - (w+h-math.gcd(w,h)) 
-
-
-# def gcd(a,b): return b if (a==0) else gcd(b%a,a) 
--- 4.py
+++
@@ -1,6 +0,0 @@
-def solution(n):
-    answer=''
-    while n>0:
-        n,b= divmod(n-1,3)
-        answer="124"[b]+answer 
-    return answer
