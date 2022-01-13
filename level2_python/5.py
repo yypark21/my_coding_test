92424c302a1bf86138b48ee9770985a2e31ecbe3
unknown <yypark1695@gmail.com>
2022-01-13 17:47:40 +0900

    5

--- level2_python/5.py
+++ level2_python/5.py
@@ -1,2 +1,23 @@
-i:=2010
-print(i:=2010)
+def solution(progresses, speeds):
+
+    answer = []
+    time = 0
+    count = 0
+    
+    while len(progresses)> 0:
+        if (progresses[0] + time*speeds[0]) >= 100: 
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+
+    return answer
+
+
+
+
