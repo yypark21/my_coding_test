7f1ecc5a84c6d1e76298415b423e8432ae90fb9e
YY Park <96404926+yypark21@users.noreply.github.com>
2022-03-18 15:36:20 +0900

    Update xml_bndbox2.py

--- xml_bndbox2.py
+++ xml_bndbox2.py
@@ -1,9 +1,10 @@
 import xml.etree.ElementTree as ET
 import glob
 import copy
+import os
 
 for targetDir in sorted(glob.glob('./*.xml')):
-    print(targetDir)
+
     xml_file = open((targetDir), 'rt', encoding='UTF8')
     anno = ET.parse(xml_file)
     annotation = anno.getroot()
@@ -18,7 +19,10 @@ for targetDir in sorted(glob.glob('./*.xml')):
         
         annotation.append(copy_xy)
         tree = ET.ElementTree(annotation)
-        tree.write(targetDir)
+        # tree.write(targetDir) # 파일 이름 그대로
+        
+        file_path = os.path.splitext(targetDir)[0]
+        tree.write(file_path+'new.xml')
 
     
     
